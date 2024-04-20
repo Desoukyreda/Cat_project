b007941e11421124f6d6710cd218acb9e04d9b88
Desouky Reda <desoukyreda@std.mans.edu.eg>
2024-04-20 22:33:24 +0200

    add some modify

---
+++ Student_record/Admin.h
@@ -0,0 +1,8 @@
+#ifndef ADMIN_H_INCLUDED
+#define ADMIN_H_INCLUDED
+
+#define default_pass 1234
+
+int began();
+
+#endif // ADMIN_H_INCLUDED
--- Student_record/main.c
+++ Student_record/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include"student.h"
+#include"Admin.h"
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int mode = began();
+
+    student_info *st;
+    switch(mode)
+    {
+    case 1:
+     admin();
+     break;
+    case 2:
+     student();
+     break;
+    }
 }
